676f9698e176b58e05f4ed70318cd9ec50951bbe
codepo8 <chris.heilmann@gmail.com>
2020-11-02 19:12:32 +0100

    added table filters

--- csdb-preview.js
+++ csdb-preview.js
@@ -6,59 +6,56 @@ javascript:(function(){document.body.appendChild(document.createElement('script'
 */
 
 (function(){
-  if (document.querySelector('alt-swab')) {
+  if (document.querySelector('csdb-preview')) {
     return;
   }
-  class altSwab extends HTMLElement {
+  class CSDBOverlay extends HTMLElement {
     constructor () {
       super();
     }
     static get observedAttributes() {
-      return ['error','alttext','reviews','hidden'];
+      return ['alttext','reviews','hidden'];
     }
-    get error() {
-      return this.hasAttribute('error');
-    }
-    get alttext() {
-      return this.hasAttribute('alttext');
-    }
-    get reviews() {
-      return this.hasAttribute('reviews');
-    }
-    get hidden() {
-      return this.hasAttribute('hidden');
-    }
+
+    get alttext() {return this.hasAttribute('alttext');}
+    get reviews() {return this.hasAttribute('reviews');}
+    get hidden() {return this.hasAttribute('hidden');}
+
     attributeChangedCallback(name, oldValue, newValue) {
+
       if(this.shadowRoot){
-        if (this.error) {
-          this.shadowRoot.querySelector('div').classList.add('error');
-        } else {
-          this.shadowRoot.querySelector('div').classList.remove('error');
-        }
+        
+        let div = this.shadowRoot.querySelector('div');
+        let img = this.shadowRoot.querySelector('.image');
+        let reviews = this.shadowRoot.querySelector('.reviews');
+
         if (this.hidden) {
-          this.shadowRoot.querySelector('div').classList.add('hidden');
+          div.classList.add('hidden');
         } else {
-          this.shadowRoot.querySelector('div').classList.remove('hidden');
+          div.classList.remove('hidden');
         }
 
         if (this.alttext) {
-          this.shadowRoot.querySelector('.image').innerHTML = this.getAttribute('alttext');
+          img.innerHTML = this.getAttribute('alttext');
         } else {
-          this.shadowRoot.querySelector('.image').innerHTML = ''
+          img.innerHTML = ''
         }
+
         if (this.reviews) {
-          this.shadowRoot.querySelector('.reviews').innerHTML = this.getAttribute('reviews');
+          reviews.innerHTML = this.getAttribute('reviews');
         } else {
-          this.shadowRoot.querySelector('.reviews').innerHTML = ''
+          reviews.innerHTML = ''
         }
       }
     }
+
     connectedCallback () {
       let shadowRoot = this.attachShadow({mode: 'open'});
       shadowRoot.innerHTML = `
         <style>
         div {
           position: fixed;
+          right: 20px;
           background: #3d6ab7;
           font-family: Sans-serif;
           max-width: 400px;
@@ -66,7 +63,6 @@ javascript:(function(){document.body.appendChild(document.createElement('script'
           min-width: 350px;
           overflow: scroll ;
           top: 10px;
-          left: 10px;
           font-size: 16px;
           color:black;
           box-shadow:3px 3px 20px #333;
@@ -91,10 +87,6 @@ javascript:(function(){document.body.appendChild(document.createElement('script'
         .hidden {
           display: none;
         }
-        div.error {
-          border: 5px solid firebrick;
-        }
-
         div button {
           border: none;
           font-size: 18px;
@@ -147,6 +139,7 @@ javascript:(function(){document.body.appendChild(document.createElement('script'
         document.addEventListener('mouseup', enddrag);
         document.addEventListener('mousemove', startdrag);
       }
+
       const startdrag = (ev) => {
         ev = ev || window.event;
         ev.preventDefault();
@@ -154,8 +147,10 @@ javascript:(function(){document.body.appendChild(document.createElement('script'
         swatchy = mousey - ev.clientY;
         mousex = ev.clientX;
         mousey = ev.clientY;
-        shadowRoot.querySelector('div').style.top = (shadowRoot.querySelector('div').offsetTop - swatchy) + "px";
-        shadowRoot.querySelector('div').style.left = (shadowRoot.querySelector('div').offsetLeft - swatchx) + "px";
+        shadowRoot.querySelector('div').style.top = 
+          (shadowRoot.querySelector('div').offsetTop - swatchy) + "px";
+        shadowRoot.querySelector('div').style.left = 
+          (shadowRoot.querySelector('div').offsetLeft - swatchx) + "px";
       }
       const enddrag = _ => {
         document.removeEventListener('mouseup', enddrag);
@@ -164,75 +159,70 @@ javascript:(function(){document.body.appendChild(document.createElement('script'
       shadowRoot.querySelector('h1').addEventListener('mousedown', initiatedrag);
     }
   }
-  window.customElements.define('alt-swab', altSwab);
+  window.customElements.define('csdb-preview', CSDBOverlay);
 
+  let csdbPreview = document.createElement('csdb-preview');
+  document.body.appendChild(csdbPreview);
 
-  let altDisplay = document.createElement('alt-swab');
-  document.body.appendChild(altDisplay);
-  altDisplay.setAttribute(
+  csdbPreview.setAttribute(
     'alttext',
     'Roll over any release link<br>Drag to where you want me'
   );
-  altDisplay.removeAttribute('hidden');
 
-  const outimg = e => {
-    altDisplay.setAttribute(
-      'alttext',
-      ''
-    );
-    altDisplay.setAttribute(
-      'reviews',
-      ''
-    );
-};
+  csdbPreview.removeAttribute('hidden');
 
-  const overimg = e => {
+  const outlink = e => {
+    csdbPreview.setAttribute('alttext', '');
+    csdbPreview.setAttribute('reviews', '');
+  };
+
+  const overlink = e => {
+
     let id = +e.target.href.replace(/.*\?id=/,'');
     let folderid = '1';
-    if (id >= 1000 && id < 10000) {
-      folderid = ('' + id).slice(0,1) + '000';
-    }
-    if (id >= 10000 && id < 100000) {
-      folderid = ('' + id).slice(0,2) + '000';
-    }
-    if (id >= 100000) {
-      folderid = ('' + id).slice(0,3) + '000';
-    }
+    let chunk = 0;
+    if (id >= 1000 && id < 10000) { chunk = 1 }
+    if (id >= 10000 && id < 100000) { chunk = 2 }
+    if (id >= 100000) { chunk = 3 }
+    folderid = ('' + id).slice(0,chunk) + '000';
+    
     let url = `https://csdb.dk/gfx/releases/${folderid}/${id}.png`;
-
     fetch(url, { method: 'HEAD' })
     .then(res => {
         if (res.ok) {
           out = `<img src="${url}">`;
-          altDisplay.setAttribute('alttext', out);
+          csdbPreview.setAttribute('alttext', out);
         } else {
           url = url.replace('.png','.gif');
           out = `<img src="${url}">`;
-          altDisplay.setAttribute('alttext', out);
+          csdbPreview.setAttribute('alttext', out);
         }
     }).catch(err => console.log('Error:', err));
-
-    // https://csdb.dk/release/?id=182085
-    // https://csdb.dk/gfx/releases/182000/182085.png
 
     fetch(e.target.href)
       .then(response => response.text())
       .then(data => {
-        altDisplay.setAttribute(
+        csdbPreview.setAttribute(
         'reviews',
         ''
     );
     if (data.indexOf('<a name="review">') === -1) {
-        altDisplay.setAttribute('reviews','No comments');
+        csdbPreview.setAttribute('reviews','No comments');
     } else {
       data = data.replace(/\n/g,'');
       let html = data.replace(/.*<b>User Comment/,'');
       html = html.replace(/Add a <a href="\/release\/addcomment.*/,'');
+
+      // invoke new DOM Parser
       let parser = new DOMParser();
-      let htmlDoc = parser.parseFromString(html, 'text/html');
-      let authors = [...htmlDoc.querySelectorAll('a[href*=scener]')];
-      let comments = [...htmlDoc.querySelectorAll('table')];
+      // parse as HTML, this allows you to use querySelector
+      let parsed = parser.parseFromString(html, 'text/html');
+      // casting to arrays [... ] makes it easier as you get 
+      // slice, filter, map and so on, but might be slow (!)
+      let authors = [...parsed.querySelectorAll('a[href*=scener]')];
+      let comments = [...parsed.querySelectorAll('table')];
       if (authors.length > 3) {authors = authors.slice(0,3)};
+
       let out = '';
       authors.forEach((a,i) => {
           out += `
@@ -240,18 +230,116 @@ javascript:(function(){document.body.appendChild(document.createElement('script'
           ${comments[i].querySelector('font').innerHTML}<br><br>
         `
       })
-      altDisplay.setAttribute(
-        'reviews',
-        out
-      );
+      csdbPreview.setAttribute('reviews', out);
     }
   });
   };
 
-  let alllinks = document.querySelectorAll('a');
-  alllinks = [...alllinks].filter(a => {return a.href.indexOf('release/?')!==-1})
-  alllinks.forEach(i => {
-    i.addEventListener('mouseover', overimg);
-    i.addEventListener('mouseout', outimg);
+let releasetable = [...document.querySelectorAll('b')].filter(b => {return b.innerHTML.indexOf('Releases')!==-1})[0].nextSibling.nextSibling.nextSibling
+
+const getid = s => {
+  return s.match(/\d+/)[0];
+}
+const clean = s => {
+    s = s.replace(/<font[^>]+>/,'');
+    s = s.replace(/<\/font>/,'');
+    s = s.replace(/&nbsp;/,'');
+    
+  return s;
+}
+let table = releasetable.querySelectorAll('tr');
+let data = {
+  release: [],
+  page: [],
+  dl: [],
+  year: [],
+  type: [],
+  accolade: []
+};
+table.forEach(tr => {
+  let tds = tr.querySelectorAll('td');
+  let as = tr.querySelectorAll('a');
+  let link = [...as].filter(a => {return a.href.indexOf('/release/?')!==-1});
+  data.page.push(getid(link[0].href));
+  data.release.push(clean(link[0].innerHTML));
+  data.year.push(clean(tds[2].innerHTML));
+  data.type.push(clean(tds[3].innerHTML));
+  data.accolade.push(clean(tds[4].innerHTML));
+});
+let f = document.createElement('form');
+let out = 'Show: <button id="us">unselect all</button><br>';
+let i = 0;
+new Set(data.type).forEach((s) => {
+  out += `<input type="checkbox" checked id="cb${i}" value="${s}">
+          <label for="cb${i}">${s}</label>`;
+  i++;
+});
+let t = releasetable;
+t.style.display = 'none';
+t.parentNode.insertBefore(f,t);
+f.innerHTML = out;
+let container = document.createElement('table');
+container.id = 'sortrelease';
+t.parentNode.insertBefore(container,t);
+f.addEventListener('change', e => {
+  rendertable();
+});
+const rendertable = _ => {
+  let showitems = [];
+  let cbs = document.querySelectorAll('form input[type=checkbox]');
+  cbs.forEach(c => {
+    if (c.checked) {showitems.push(c.value)}
   });
+  let out = `
+    <tr>
+      <th>Name</th>
+      <th>Year</th>
+      <th>Type</th>
+      <th>Compo</th>
+    </tr>`;
+  data.release.forEach((name,i) => {
+    if (showitems.indexOf(data.type[i])!==-1){
+      out += `<tr>
+      <td><a href="/release/?id=${data.page[i]}">${name}</a></td>
+      <td>${data.year[i]}</td>
+      <td>${data.type[i]}</td>
+      <td>${data.accolade[i]}</td>
+      </tr>`
+    }
+  })
+  container.innerHTML = out;
+  let alllinks = container.querySelectorAll('a');
+  alllinks = [...alllinks].filter(
+    a => { return a.href.indexOf('release/?')!==-1 }
+  );
+  alllinks.forEach(a => {
+    a.addEventListener('mouseover', overlink);
+    a.addEventListener('mouseout', outlink);
+  });
+}
+rendertable();
+document.querySelector('#us').addEventListener('click',e => {
+  f.querySelectorAll('input[type=checkbox]').forEach(cb => {cb.checked = false});
+  rendertable();
+  e.preventDefault();
+})
+let st = document.createElement('style');
+document.head.appendChild(st);
+st.innerHTML = `
+   #sortrelease td, #sortrelease th {
+    padding: 5px 3px;
+  }
+  #sortrelease {
+   width: 100%;
+  }
+  #sortrelease th {
+    background: #7474fc;
+  }
+  @media (min-width:600px) {
+    html body td {
+      font-size: 1.2em;
+    }
+  }
+`;
+
 })();
